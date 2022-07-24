ce237e244088d32a4d926342d2c74a5f44f439c3
Andrew Baker <andrew.baker0007@gmail.com>
2022-07-24 17:40:56 -0400

    Added name, more minor formatting

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,36 @@ import Babysitter from "./components/Babysitter.vue";
 <template>
   <header>
     <h1>Northwoods Babysitter Kata</h1>
+    <h3>by Andrew Baker</h3>
   </header>
   <main>
     <div id="BabySitterDiv">
-      <Babysitter />
+      <Babysitter/>
     </div>
   </main>
 </template>
 
 <style scoped>
-
   header {
+    background-color: aqua;
+    
+  }
+
+  h1 {
     background-color: aqua;
     color: black;
     -webkit-text-fill-color: white;
     -webkit-text-fill-color: white;
     -webkit-text-stroke-width: .5px;
     font-family: 'Impact';
-    padding-left: 2%;
+    padding-left: 10px;
+    padding-top: 5px;
+  }
+
+  h3 {
+        padding-left: 10px;
+        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        padding-bottom: 10px;
   }
 
   #BabySitterDiv {
